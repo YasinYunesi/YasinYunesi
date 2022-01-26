6d9953a5290aa1a9a8fcd8801fd7524c827e65ed
YasinYunesi <yasinyunesi80@gmail.com>
2022-01-27 00:00:36 +0330

    Added CV 📃

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,5 @@
+import Resume from "../Assets/resume.pdf";
+
 const Navbar = () => {
   return (
     <>
@@ -17,7 +19,7 @@ const Navbar = () => {
 
         {/* navbar content */}
         <div className='navbar_content collapse navbar-collapse' id='navbarContent'>
-          <ul className='navbar_nav navbar-nav col-8 mx-auto py-1'>
+          <ul className='navbar_nav navbar-nav col-7 mx-auto py-1'>
             <a className='nav-link text-uppercase me-5 nav_link_active' href='#banner'>
               Home
             </a>
@@ -34,6 +36,14 @@ const Navbar = () => {
               contact
             </a>
           </ul>
+          {/* Download CV */}
+          <div className='text-end py-2 pe-3 pe-lg-5'>
+            <button className='download_cv fw-bold rounded-pill px-0 py-2 overflow-hidden'>
+              <a className='py-3 px-5' href={Resume} download>
+                Download CV
+              </a>
+            </button>
+          </div>
         </div>
       </nav>
     </>
